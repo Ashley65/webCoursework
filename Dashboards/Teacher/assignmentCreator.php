604c4c5be65850fa1bd568b71c77e6dc83f4b247
Tom Work <22007514@hope.ac.uk>
2024-02-28 18:45:18 +0000

    Dark mode and Kind of calendar

--- Dashboards/Teacher/assignmentCreator.php
+++ Dashboards/Teacher/assignmentCreator.php
@@ -6,7 +6,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Assignment Maker</title>
-    <link rel="stylesheet" href="styles.css">
     <script src="../../https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../../js/script.js"></script>
     <script src="../../js/theme.js"></script>
