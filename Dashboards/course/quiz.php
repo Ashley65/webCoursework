45eff96d48b7a1e2c3c6fbee09ee22b626b76efc
Ashley65 <nchinembiri@outlook.com>
2024-03-01 11:54:27 +0000

    course without any backend

---
+++ Dashboards/course/quiz.php
@@ -0,0 +1,86 @@
+<?php
+
+?>
+
+<! DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= device-width, initial-scale=1.0">
+    <title>Student Dashboard</title>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="../../js/script.js"></script>
+    <script src="../../js/theme.js"></script>
+    <link rel="stylesheet" href="../../assets/dashboard_css/dark-light.css">
+    <link rel="stylesheet" href="../../assets/dashboard_css/Dashboard.css">
+    <link rel="stylesheet" href="../../assets/dashboard_css/sidebar.css">
+    <link rel="stylesheet" href="../../assets/dashboard_css/top-bar.css">
+    <link rel="stylesheet" href="../../assets/course_css/course.css">
+    <link rel="stylesheet" href="../../assets/gridlayout_css/gridLayoutForCourseInfo.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+</head>
+
+<body>
+<div class="container">
+    <aside class="aside">
+        <div class="toggle">
+            <div class="logo">
+                <img src="../../assets/img/AceTraining-logo-light-transparent.png" alt="">
+            </div>
+            <button class="menu-btn" id="toggleBtn">
+                <span class="material-symbols-outlined">menu</span>
+            </button>
+
+        </div>
+        <div class="sidebar">
+            <a href="../Student/StudentMain.php" >
+                <span class="material-symbols-outlined">home</span>
+                <h3>Home</h3>
+            </a>
+            <a href="../Student/profile.php" >
+                <span class="material-symbols-outlined">person</span>
+                <h3>Profile</h3>
+            </a>
+            <a href="../Student/course.php" class="active">
+                <span class="material-symbols-outlined">book</span>
+                <h3>Course</h3>
+            </a>
+            <a href="../Student/assignment/assignment.php">
+                <span class="material-symbols-outlined">assignment</span>
+                <h3>Assignment</h3>
+            </a>
+            <a href="../Student/timetable.php">
+                <span class="material-symbols-outlined">today</span>
+                <h3>Timetable</h3>
+            </a>
+            <a href="../Student/calendar.php">
+                <span class="material-symbols-outlined">calendar_month</span>
+                <h3>Calendar</h3>
+                <a href="../../LoginSystem/logout.php">
+                    <span class="material-symbols-outlined">logout</span>
+                    <h3>Logout</h3>
+                </a>
+        </div>
+
+    </aside>
+    <div class="Top-bar">
+        <div class="nav">
+
+            <button  id="toggleBtn">
+                <span class="material-symbols-outlined">menu</span>
+            </button>
+            <div class="toggleTheme" id="mode-toggle"></div>
+            <div class="profile">
+                <div class="info">
+                    <p>hey, <?php echo isset($user) ? $user ['FName']: 'Guest';?></p>
+                    <small class="textMuted">Student</small>
+                </div>
+                <div class="profile_pic">
+                    <img src="../../assets/img/Default_pfp.png">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
